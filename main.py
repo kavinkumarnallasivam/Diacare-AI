5c27a46efd5576d764cbec42c1393f021c2e5832
kavinkumarnallasivam <kavinkumarnallasivam@gmail.com>
2026-01-11 13:14:41 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+import joblib
+import numpy as np
+
+app = FastAPI()
+
+model = joblib.load("model/diabetes_risk_model.pkl")
+
+@app.get("/")
+def health():
+    return {"status": "API running"}
+
+@app.post("/predict")
+def predict(data: dict):
+    features = np.array([[
+        data["avg_glucose"],
+        data["carbs_g"],
+        data["adherence_pct"],
+        data["activity_min"]
+    ]])
+
+    pred = model.predict(features)[0]
+    prob = model.predict_proba(features)[0][pred]
+
+    return {
+        "risk": int(pred),
+        "confidence": round(float(prob), 2)
+    }
